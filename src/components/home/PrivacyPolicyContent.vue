902ba9e866254179d9fadb62fc6c9048dd7b7b71
Kongsittha Nimking <casnova152@gmail.com>
2024-05-31 21:36:48 +0700

    save

--- src/components/home/PrivacyPolicyContent.vue
+++ src/components/home/PrivacyPolicyContent.vue
@@ -1,3 +1,18 @@
 <template>
-  <div class="text-6xl text-gray-800">Privacy Policy</div>
+  <div class="flex flex-col space-y-10 m-20">
+    <div class="law-text-header">Terms of Service</div>
+    <div class="law-text-paragraph">Effective Date: May 31, 2024</div>
+    <div class="law-text-header">AGREEMENT TO OUR LEGAL TERMS</div>
+    <div class="law-text-paragraph">
+      We are Mendoka ("Company," "we," "us," "our"), a company registered in
+      Thailand, 108/1 Moo 9 Nakorn Chedi, Pa Sang, Lamphun, TX 51120.
+    </div>
+    <div class="law-text-paragraph">
+      These Terms of Service and End User License Agreement (the “Terms”) are an
+      agreement between you and Mendoka. They govern your access and use of the
+      websites, games, mobile apps, and other interactive software products and
+      services that we operate and that link to these Terms (collectively, our
+      “Services”).
+    </div>
+  </div>
 </template>
