d614358b8cf9e09307feaf7ef2ec0fb7a9be4b92
Kongsittha Nimking <casnova152@gmail.com>
2024-06-06 23:29:46 +0700

    Update HomeView.vue

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -38,6 +38,7 @@ onMounted(() => {
     </video>
     <!-- Background Video -->
     <video
+      v-if="isCompleteOpening"
       class="absolute top-0 left-0 w-full h-full object-cover object-[-280px] xl:object-center"
       autoplay
       muted
